4d03e12f4c00d9e36fa971e558d02a9dcddbe656
Jorge R F Borges <jorge.borges@parceiro.gruposaltaedu.com>
2023-10-14 15:04:13 -0300

    first commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+
+            return Ok();
+        }
+    }
+}
+
